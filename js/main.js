df778db24b3b33c794ca800c240c862438a8c3dd
Rocio Yovera <rocioyovera@ gmail.com>
2017-01-13 13:04:25 -0500

    finalizado

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+
+var h2 = document.getElementById ("form-signin-heading");
+h2.innerHTML = "Por favor inicia sesión";
+
+var input = document.getElementById ("inputEmail");
+input.placeholder = "Correo Electrónico";
+
+var input = document.getElementById ("inputPassword");
+input.placeholder = "Contraseña";
+
+document.querySelector('span').innerHTML="Recordar datos";
+
+document.querySelector('button').innerHTML="Iniciar sesión";
